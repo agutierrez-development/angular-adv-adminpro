f642baee98c9217cad644762e51bf504142d7642
Arturo Gutiérrez <agutierrez.development@gmail.com>
2022-10-27 23:09:30 -0500

    Fin seccion 3 - diseño

---
+++ src/app/pages/grafrica1/grafrica1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafrica1',
+  templateUrl: './grafrica1.component.html',
+  styles: [
+  ]
+})
+export class Grafrica1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
